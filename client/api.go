1d7714858b50f589c1823a3381fc8fe1521e3534
markc <mark.c@binance.com>
2023-01-11 16:24:34 +0400

    add get rollbacks for marketplace

--- client/api.go
+++ client/api.go
@@ -121,6 +121,9 @@ type ZkBNBQuerier interface {
 	// GetNftsByAccountIndex returns nfts by account index
 	GetNftsByAccountIndex(accountIndex, offset, limit int64) (*types.Nfts, error)
 
+	// GetRollbacks returns tx rollback info
+	GetRollbacks(fromBlockHeight, offset, limit int64) (total uint32, rollbacks []*types.Rollback, err error)
+
 	// GetNftByTxHash returns nfts by txHash
 	GetNftByTxHash(txHash string) (*types.NftIndex, error)
 
